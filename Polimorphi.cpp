1ba9156126fa91da65e695cc04cf4d7763fc38c8
Bendadiotulada-323 <bendadiotulada@gmail.com>
2026-05-18 15:19:06 +0700

    Polimorphi file

---
+++ Polimorphi.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0; // pure virtual function
+    // virtual void pesan() {
+    //     cout << "Hallo saya seseorang\n" << endl;
+    // }
+
+};
+
+class joko : public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari joko\n" << endl;
+    }
+
+};
+
+class lia : public seseorang {
+public:
+    voiid pesan() {
+        cout << "Pesan dari lia\n" << endl;
+    }
+};
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    //b.seseorang::pesan();
+    return 0;
+}
